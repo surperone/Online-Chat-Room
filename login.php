39880a26dfc8f7db198c5c192af34ebe332199cd
aprikyblue <aprikyblue@gmail.com>
2015-08-18 01:02:46 +0800

    增加对空昵称的检测

--- login.php
+++ login.php
@@ -3,8 +3,13 @@ session_start();
 if ( isset( $_POST['nick'] ) )
 {
 	//处理登陆请求
-	$_SESSION['nick'] = $_POST['nick'];
-	header("Location: index.php");
+	if (!$_POST['nick'])
+	{
+		header("Location: login.php");
+	}else{
+		$_SESSION['nick'] = $_POST['nick'];
+		header("Location: index.php");
+	}
 }else{
 	//显示登陆页面
 	include 'login.html';
