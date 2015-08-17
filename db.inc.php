41588323410e0f837ac46982ba78353a60af19bf
aprikyblue <aprikyblue@gmail.com>
2015-08-18 00:12:54 +0800

    更新数据库配置

--- db.inc.php
+++ db.inc.php
@@ -3,11 +3,13 @@
 //初始化常量
 define('DEBUG', true);
 define('PCONNECT', function_exists('mysql_pconnect'));
-define('MYSQL_HOST', '127.0.0.1:3306');
-define('MYSQL_USER', 'root');
-define('MYSQL_PWD', '123456');
-//define('MYSQL_USER', 'admin');
-//define('MYSQL_PWD', 'b91c750b');
+if (!defined('MYSQL_HOST'))
+{
+	define('MYSQL_HOST', '127.0.0.1');
+	define('MYSQL_PORT', '3306');
+	define('MYSQL_USER',  'root');
+	define('MYSQL_PWD',  '123456');
+}
 
 //调试处理
 if(DEBUG){
@@ -17,9 +19,9 @@ if(DEBUG){
 //初始化数据库
 if(PCONNECT){
 	//使用持久连接
-	$link = @mysql_pconnect(MYSQL_HOST, MYSQL_USER, MYSQL_PWD) or die('Database connect failed');
+	$link = @mysql_pconnect(MYSQL_HOST.':'.MYSQL_PORT, MYSQL_USER, MYSQL_PWD) or die('Database connect failed');
 }else{
-	$link = @mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PWD) or die('Database connect failed'); 
+	$link = @mysql_connect(MYSQL_HOST.':'.MYSQL_PORT, MYSQL_USER, MYSQL_PWD) or die('Database connect failed'); 
 }
 mysql_select_db("chat");
 mysql_query('set names utf8');
