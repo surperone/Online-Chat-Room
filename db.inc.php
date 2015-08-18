81044236fe5ecdc9f6a370160add78721fe4a5b2
aprikyblue <aprikyblue@gmail.com>
2015-08-18 14:10:56 +0800

    Signed-off-by: aprikyblue <aprikyblue@gmail.com>

--- db.inc.php
+++ db.inc.php
@@ -3,13 +3,10 @@
 //初始化常量
 define('DEBUG', true);
 define('PCONNECT', function_exists('mysql_pconnect'));
-if (!defined('MYSQL_HOST'))
-{
-	define('MYSQL_HOST', '127.0.0.1');
-	define('MYSQL_PORT', '3306');
-	define('MYSQL_USER',  'root');
-	define('MYSQL_PWD',  '123456');
-}
+define('MYSQL_HOST', '127.0.0.1');
+define('MYSQL_PORT', '3307');
+define('MYSQL_USER',  'admin');
+define('MYSQL_PWD',  '1b9ef7cf');
 
 //调试处理
 if(DEBUG){
